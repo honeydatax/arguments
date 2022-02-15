1947418fa5c4f74397beb560350fe426cf7cd34a
lisbon <16432726+honeydatax@users.noreply.github.com>
2022-02-15 16:02:10 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 printf "\ec\e[46;34m\ainstalling...\n"
-gcc -o arguments arguments.c
+gcc -o arguments arguments.c -lX11
 gcc -o arglists arglist.c
 sudo cp arguments /usr/bin
 sudo cp arguments.desktop /usr/share/applications
